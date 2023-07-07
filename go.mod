71878a08c3ccacc516888574bc9d0cf7ab9ae6e8
Omar Eqbal <omar.eqbal@nutanix.com>
2023-07-07 11:35:35 +0000

    init go module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/eqbalomar/vendor_update_assignment
+
+go 1.17
+
+require github.com/gofrs/uuid v4.4.0+incompatible
---
+++ main.go
@@ -0,0 +1,11 @@
+package example
+
+import (
+	"fmt"
+
+	_ "github.com/gofrs/uuid"
+)
+
+func PrintHello() {
+	fmt.Println("Custom module for vendor update")
+}
